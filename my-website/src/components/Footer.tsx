0d4b0e47661838e8c58f2a2c01f68d7117e9e38e
SKeana <Skean2000@outlook.com>
2025-05-06 08:30:55 +0100

    restart again

--- my-website/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, Text } from '@chakra-ui/react';
-
-export default function Footer() {
-  return (
-    <Box as="footer" bg="gray.800" color="white" py={6} textAlign="center">
-      <Text>© 2025 My Portfolio. All rights reserved.</Text>
-    </Box>
-  );
-}
